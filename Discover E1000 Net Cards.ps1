b31226958e430963d306b6f28462fedf3d39e5ec
Amitk3293 <75209120+Amitk3293@users.noreply.github.com>
2020-12-27 16:00:57 +0200

    Add files via upload

---
+++ Discover E1000 Net Cards.ps1
@@ -0,0 +1,5 @@
+#Discover and export all the E1000 NICs
+
+
+Get-VM | Get-NetworkAdapter | Where-object {$_.Type -ne "Vmxnet3"} | Select @{N="VM";E={$_.Parent.Name}},Name,Type |
+export-Csv  c:\Network_Interface.csv -NoTypeInformation
